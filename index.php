d8efcf5c1942145feb598c389e16dc65d324cdf1
divinity76 <divinity76@gmail.com>
2014-05-15 16:11:49 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_GET['test'])){
+require('index_current_test.php');
+	}else{
+require("index_current.php");
+}
+/*
+require("index_nissan4x4_old.html");
+require("index_old.html");
+require("index_oldold.html");
+*/
+
+?>
